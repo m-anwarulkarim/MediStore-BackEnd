85852d65d7228eb54ac492c7ef8300b5f1056867
m-anwarulkarim <dev.anwarul@gmail.com>
2026-01-27 22:01:35 +0600

    authGuard added

--- src/module/auth/auth.route.ts
+++ src/module/auth/auth.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { userController } from "./auth.controller";
+import authGuard from "../../guard/auth.guard";
+import { ROLE } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-// Example route
-router.get("/user", userController.createUser);
+router.get(
+  "/user",
+  authGuard(ROLE.ADMIN, ROLE.SELLER, ROLE.CUSTOMER),
+  userController.viewAllUsers,
+);
+router.get("/user/:id", userController.viewSingleUser);
 
 export const userRouter = { router };
